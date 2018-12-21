f11d05ee086a70a155e9cf2cc9e32c0ff289cd22
Naïm Favier <fnaim42@gmail.com>
2018-12-21 01:28:55 +0100

    Add files

---
+++ config.dist.sh
@@ -0,0 +1,12 @@
+# The server and port to connect to (port defaults to 6667)
+server=
+port=
+
+# The password for identifying to services (leave empty to disable)
+password=
+
+# The channels to join
+channels=()
+
+# The prefix for bot commands
+prefix='.'
